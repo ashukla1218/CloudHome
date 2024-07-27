84e609bb751319823661ad6ad427be996b1c403f
Aashutosh Shukla <aashu.guru0@gmail.com>
2024-07-27 15:29:14 +0530

    deleted unnecessary css and updated otp page

--- CloudHome/frontend/src/pages/otpPage.jsx
+++ CloudHome/frontend/src/pages/otpPage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/navbar";
 import { useSelector } from "react-redux";
 import useGenerateNewOtp from "../hooks/useGenerateNewOtp";
 import useVerifyOtp from "../hooks/useVerifyOtp";
+import logo from '../../src/pages/logo.png';
 
 const OtpPage = () => {
     const { email } = useSelector((e) => e.auth);
@@ -32,7 +33,11 @@ const OtpPage = () => {
             <Navbar />
             <div className="auth-page-wrapper">
             <div className="otp-page-container">
+            <img src={logo} alt="Logo" className="otp-logo" />
+                <h1>OTP Verification</h1>
+            <div className="email-container">
                 <p>Email : {email}</p>
+            </div>
                 <div className="otp-input-container">
                     <input maxLength={4} type="text" value={otp} onChange={(e) => setOtp(e.target.value)} />
                     <div className="otp-column c1" />

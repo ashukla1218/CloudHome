33164beb9680a2a4c98637ffe81ce046596841e5
Aashutosh Shukla <aashu.guru0@gmail.com>
2024-07-28 08:53:15 +0530

    backend changes for file uploading

--- CloudHome/backend/config/uploadFileMulter.js
+++ CloudHome/backend/config/uploadFileMulter.js
@@ -1,14 +1,9 @@
 const multer = require("multer");
 
-// Set up storage for uploaded files
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads/");
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${Math.floor(Math.random() * 100)}-${file.originalname}`);
-    },
-});
+
+const storage = new multer.memoryStorage();
+
+
 
 // Create the multer instance
 module.exports = multer({ storage: storage });
--- CloudHome/backend/controller/fileControllers.js
+++ CloudHome/backend/controller/fileControllers.js
@@ -2,6 +2,7 @@ const cloudinary = require("../config/cloudinary");
 const FileFolderModel = require("../model/fileSchema");
 const fsPromises = require("fs/promises");
 
+
 const createFileDocumentInMongoDB = async (req, res) => {
     try {
         const data = req.file;
@@ -38,68 +39,63 @@ const createFileDocumentInMongoDB = async (req, res) => {
     }
 };
 
-const uploadFileToCloudinary = async (file) => {
+const uploadFileToCloudinary = async (req,file) => {
     try {
-        const result = await cloudinary.uploader.upload(file.metaData.multer.path, {
-            folder: `Cloud-Home/${file.userId}/${file.parentId}`,
-            timeout: 60000,
+      const b64 = Buffer.from(req.file.buffer).toString("base64");
+      let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+      const result =  await cloudinary.uploader.upload(dataURI, {
+          resource_type: "auto",
+          folder: `Cloud-Home/${file.userId}/${file.parentId}`,
+          timeout: 60000,
         });
-
-        try {
-            await FileFolderModel.findByIdAndUpdate(file._id, {
-                link: result.secure_url || result.url,
-                "metaData.cloudinary": result,
-            });
-
-            return true;
-        } catch (err) {
-            console.log("---------------------------------");
-            console.log("❌❌❌❌ File UPDATE Error ❌❌❌❌");
-            console.log(err);
-            console.log("---------------------------------");
-            return false;
-        }
-    } catch (err) {
+  
+      try {
+        await FileFolderModel.findByIdAndUpdate(file._id, {
+          link: result.secure_url || result.url,
+          "metaData.cloudinary": result,
+        });
+  
+        return true;
+      } catch (err) {
         console.log("---------------------------------");
-        console.log("❌❌❌❌ Cloudinary Error ❌❌❌❌");
+        console.log("❌❌❌❌ File UPDATE Error ❌❌❌❌");
         console.log(err);
         console.log("---------------------------------");
         return false;
-    }
-};
-
-const deleteFileFromServer = async (file) => {
-    try {
-        await fsPromises.rm(file.metaData.multer.path);
-        console.log("File deleted ✅");
+      }
     } catch (err) {
-        console.log("---------------------------------");
-        console.log("❌❌❌❌ File Deletion from Server Failed ❌❌❌❌");
-        console.log(err);
-        console.log("---------------------------------");
-        return false;
+      console.log("---------------------------------");
+      console.log("❌❌❌❌ Cloudinary Error ❌❌❌❌");
+      console.log(err);
+      console.log("---------------------------------");
+      return false;
     }
-};
+  };
+  
+
+
 
 const createFile = async (req, res) => {
     try {
-        const documentCreated = await createFileDocumentInMongoDB(req, res);
-        if (documentCreated) {
-            const isFileUploadedToCloudinary = await uploadFileToCloudinary(documentCreated);
-            if (isFileUploadedToCloudinary) {
-                deleteFileFromServer(documentCreated);
-            }
+      const documentCreated = await createFileDocumentInMongoDB(req, res);
+      if (documentCreated) {
+        const isFileUploadedToCloudinary = await uploadFileToCloudinary(
+          req,
+          documentCreated
+        );
+        if (isFileUploadedToCloudinary) {
+          // deleteFileFromServer(documentCreated);
         }
+      }
     } catch (err) {
-        console.log("------------------------");
-        console.log(err);
-        console.log("------------------------");
-        res.status(500).json({
-            status: "fail",
-            message: "Internal Server Error",
-        });
+      console.log("------------------------");
+      console.log(err);
+      console.log("------------------------");
+      res.status(500).json({
+        status: "fail",
+        message: "Internal Server Error",
+      });
     }
-};
+  };
 
 module.exports = { createFile };
-
